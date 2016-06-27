76acc56eb729f8abcb4b8502a5154dd9ce719b35
eduqg <eduardoqgomes@gmail.com>
2016-06-27 09:57:17 -0300

    Taking variables separately

--- doc/TrabalhoICG/Trab_ICG_Dev/main.cpp
+++ doc/TrabalhoICG/Trab_ICG_Dev/main.cpp
@@ -19,28 +19,46 @@
 //Mesh mesh;
 
 void carregarArquivo(string nomeArquivo){
+  
   ifstream arquivo(nomeArquivo.c_str());
-  std::cout << nomeArquivo << endl;
+  cout << nomeArquivo << endl;
 
   if(!arquivo.is_open()){
     cout << "Não foi possível abrir o aquivo." << endl;
     exit(-1);
   }
   cout << "Arquivo aberto com sucesso!" << endl;
+  
+  string hash;
+   getline(arquivo, hash);
+   cout << hash << endl;
+   getline(arquivo, hash);
+   cout << hash << endl;
 
-   string tipo;
-   getline(arquivo, tipo);
+  string nome;
+   getline(arquivo, nome);
+   cout << nome << endl;
 
-   cout << tipo << endl;
+  string line;
+  string variavel;
+  float valor1, valor2, valor3;
+  getline(arquivo,line);
+  stringstream ss (line);
+  ss >> variavel >> valor1 >> valor2 >> valor3;
+
+  cout << variavel << endl;
+  cout << valor1 << endl;
+  cout << valor2 << endl;
+  cout << valor3 << endl;
+
+
+
 }
  /* Laço principal */
 int main(){
    
-
     std::string nomeArgv = "Spider-Man.obj";
-
     carregarArquivo(nomeArgv);
-
     cout << "Encerrando programa." << endl;
     
     return 0;
